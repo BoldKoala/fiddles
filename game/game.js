92f9e1e63a9b0a490f7b6453b5a06a1ac8013c65
chikeichan <chikeichan@localhost.(none)>
2015-02-13 12:20:24 -0800

    fire bullet to right direction

--- game/game.js
+++ game/game.js
@@ -1,11 +1,6 @@
-<<<<<<< HEAD
-var map = Map(300,300,3,3);
-var tank = Tank(10,10,10, 'blue', 1);
-=======
 var map = Map(30,30,3,3);
-var tank = Tank(1,1,1, 'blue', 1);
-var tankRed = Tank(1,1,1, 'red', 1);
->>>>>>> 816848c3323f4b822ea5a17e504124b9863008fe
+var tank = Tank(1,1,1, 'blue', 0.1);
+var tankRed = Tank(1,1,1, 'red', 0.1);
 var camera = new THREE.PerspectiveCamera( 75, window.innerWidth / window.innerHeight, 0.1, 1000 );
 var renderer = new THREE.WebGLRenderer();
 var WIDTH = window.innerWidth;
@@ -16,11 +11,8 @@ map.scene.add( tank.tanker );
 map.scene.add( tankRed.tanker );
 
 //Initialize Camera position
-<<<<<<< HEAD
-camera.position.y = 300;
-=======
+
 camera.position.y = 40;
->>>>>>> 816848c3323f4b822ea5a17e504124b9863008fe
 camera.position.z = 0;
 camera.lookAt({x: 0, y: 0, z: 0});
 //Set renderer size
@@ -32,12 +24,7 @@ document.body.appendChild( renderer.domElement );
 //Direction system
 
 var speed = 0;
-
 var direction = 0;
-
-var dx = 0;
-var dz = 0;
-var dy = 0;
 
 
 
@@ -48,6 +35,7 @@ window.onkeydown = function(d){
   //W key
   if(d.keyCode === 87){
     speed = -tank.speed;
+  }
   //S key
   if(d.keyCode === 83){
     speed = tank.speed;
@@ -63,7 +51,7 @@ window.onkeydown = function(d){
   //space
   if (d.keyCode === 32){
     console.log(tank.tanker.position)
-    var bullet = Bullet(1, 1, 1, tank.tanker.position);
+    var bullet = Bullet(-Math.sin(direction), -Math.cos(direction), 2, tank.tanker.position);
     map.scene.add(bullet.bulleter);
     bullet.fire();
   }
@@ -73,7 +61,7 @@ window.onkeyup = function(d){
   if(d.keyCode === 87){
     speed = 0;
   }
-  //S key
+  //S ke
   if(d.keyCode === 83){
     speed = 0;
   }
