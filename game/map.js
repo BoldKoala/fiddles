6d273a778d21de97c88598a36958ccb4c9bc1ae3
Jason Yang <jasonyang@Jasons-MacBook-Air.local>
2015-02-14 08:37:52 -0800

    draw line on the edge of map, tank can no longer move out of the map; add tank jump feature, first attpemt to fixed the 5% sync error

--- game/map.js
+++ game/map.js
@@ -23,19 +23,20 @@ function Map(x, y, step, brightness) {
 
 	// ======>> Add light source
 	map.light = new THREE.DirectionalLight(0xffffff,map.brightness);
-	map.light.position.set(0,5,0);
+	map.light.position.set(20,20,0);
 	map.scene.add(map.light);
 
 	// ====== Grid building =========
 	map.line = new THREE.Geometry();
 
-	for( var i = -map.x; i <= map.x; i += map.step ){
-	  map.line.vertices.push(new THREE.Vector3( -map.x, -0.05, i ));
-	  map.line.vertices.push(new THREE.Vector3( map.x, -0.05, i ));
-	  map.line.vertices.push(new THREE.Vector3( i, -0.05, -map.x ));
-	  map.line.vertices.push(new THREE.Vector3( i, -0.05, map.x ));
-		// map.scene.add(new THREE.Line( map.line, map.material.line, THREE.LinePieces));
+	for( var i = -map.x + 20; i <= map.x - 20; i += map.step ){
+	  map.line.vertices.push(new THREE.Vector3( -map.x + 20, -0.1, i ));
+	  map.line.vertices.push(new THREE.Vector3( map.x - 20, -0.1, i ));
+	  map.line.vertices.push(new THREE.Vector3( i, -0.1, -map.x + 20 ));
+	  map.line.vertices.push(new THREE.Vector3( i, -0.1, map.x - 20 ));
+		map.scene.add(new THREE.Line( map.line, map.material.line, THREE.LinePieces));
 	}
+	// map.scene.add(map.line)
 
 	return map;
 }
