4fd7b3ee9785bc6e0fabf2e4633b99bdb40a6ce6
Jason Yang <jasonyang@Jasons-MacBook-Air.local>
2015-02-18 13:12:28 -0800

    added towers in the map

---
+++ game/objects.js
@@ -0,0 +1,51 @@
+function createOBJ(x, y, z) {
+  var objUrl = ['./Model/objects/castle-tower.scene/DRIFTWD.JPG', './Model/objects/castle-tower.scene/wall.JPG', './Model/objects/castle-tower.scene/PLATEOX2.JPG', './Model/objects/castle-tower.scene/STUCCO8.JPG', './Model/objects/castle-tower.scene/TUTSHNGL.JPG'];
+  var OBJTexture = function(i){
+    var texture = THREE.ImageUtils.loadTexture(objUrl[i]);
+        texture.wrapS = THREE.RepeatWrapping;
+        texture.wrapT = THREE.RepeatWrapping;
+        texture.repeat.set( 0.5, 0.5 );
+
+    return new THREE.MeshLambertMaterial({ 
+      map: texture
+    })
+  }
+
+  var OBJ = {};
+
+  OBJ.testMaterial = {
+    tank: new THREE.MeshLambertMaterial({ color: "red" })
+  };
+
+  var loader = new THREE.ObjectLoader();
+  loader.load('./Model/objects/castle-tower.scene/castle-tower.json',function(OBJModel){
+      OBJ.model = OBJModel;
+      OBJ.model.scale.set(0.02, 0.02, 0.02);
+      OBJ.model.children[0].material  = OBJTexture(3);
+      OBJ.model.children[1].material  = OBJTexture(3);
+      OBJ.model.children[2].material  = OBJTexture(3);
+      OBJ.model.children[3].material  = OBJTexture(3);
+      OBJ.model.children[4].material  = OBJTexture(1);
+      OBJ.model.children[5].material  = OBJTexture(0);
+      OBJ.model.children[6].material  = OBJTexture(1);
+      OBJ.model.children[7].material  = OBJTexture(3);
+      OBJ.model.children[8].material  = OBJTexture(1);
+      OBJ.model.children[9].material  = OBJTexture(2);
+      OBJ.model.children[10].material = OBJTexture(4);
+      OBJ.model.children[11].material = OBJTexture(3);
+      // // OBJ.model.position.y += 0.5;
+      // // OBJ.model.children.forEach(function(part,i){
+      // // if (i < 5){
+      // //     OBJ.model.children[i].material = OBJTexture(i);        
+      // //   }
+      // //   // OBJ.model.children[i].scale.set(tank.x, tank.y, tank.z);
+      // // })
+      // OBJ.model.material = OBJ.testMaterial;
+      OBJ.model.position.x = x;
+      OBJ.model.position.y = y;
+      OBJ.model.position.z = z;
+      map.scene.add(OBJ.model);
+  })
+
+  return OBJ;
+}
